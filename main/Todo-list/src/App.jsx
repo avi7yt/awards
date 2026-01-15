572edc20f7e0c450c76bb275f9cd230c082c8824
animeshhq <animeshhq@gmail.com>
2026-01-15 18:21:01 +0530

    modified:   main/Todo-list/src/App.jsx 	new file:   main/Todo-list/src/pages/Checklist.jsx 	new file:   main/Todo-list/src/pages/Home.jsx 	modified:   main/Todo-list/src/pages/Login.jsx 	new file:   main/Todo-list/src/pages/Profile.jsx 	new file:   main/Todo-list/src/pages/Task.jsx

--- main/Todo-list/src/App.jsx
+++ main/Todo-list/src/App.jsx
@@ -1,39 +1,38 @@
-import styles from "./css/App.module.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import  Login from "./pages/Login"
+import  Task from "./pages/Task"
+import  Profile from "./pages/Profile"
+import  Home from "./pages/Home"
 
 function App() {
 
-  return (
-    <div className={styles.div}>
-        <h1>Create Account 👋</h1>
-        <p>Please register on our Streamline, where you can continue using our service.</p>
-    <div className={styles.form}>
-        <div className="username">
-          <input className={styles.input} type="text" name="username" id="username" placeholder="Enter your username" />
-        </div>
-
-        <div className="useremail">
-          <input className={styles.input} type="text" name="useremail" id="useremail" placeholder="Enter your email" />
-        </div>
-
-        <div className="usepassword">
-          <input className={styles.input} type="text" name="usepassword" id="usepassword" placeholder="Enter your usepassword" />
-        </div>
-    </div>
-    <div className={styles.privacy}>
-      <input type="checkbox" name="" id="" />
-      <a href="#">I agree to privacy policy & terms</a>
-    </div>
-    <div className={styles.ctabtn}>
-      <button>Continue</button>
-    </div>
-    <div className={styles.hrline}>
-      <div className={styles.line}></div>
-      <span>or</span>
-      <div className={styles.line}></div>
-    </div>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
     
+    {
+      path: "/profile",
+      element: <Profile/>,
+    },
+
+    {
+      path: "/task",
+      element: <Task/>,
+    },
+
+      {
+      path: "/login",
+      element: <Login/>,
+    },
+  ]);
+
+  return (
+  <div>
+      <RouterProvider router={router} />
   </div>
   )
-}
+};
 
-export default App
+export default App;
--- main/Todo-list/src/pages/Login.jsx
+++ main/Todo-list/src/pages/Login.jsx
@@ -1,8 +1,58 @@
-import styles from "../css/Login.module.css"
-function Login(){
-    return(
-        <div>
-            <h3 className={styles.heading}>Create Account</h3>
+import styles from "../css/App.module.css";
+
+function Login() {
+  return (
+    <div className={styles.div}>
+      <h1>Create Account 👋</h1>
+      <p>
+        Please register on our Streamline, where you can continue using our
+        service.
+      </p>
+      <div className={styles.form}>
+        <div className="username">
+          <input
+            className={styles.input}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter your username"
+          />
         </div>
-    )
+
+        <div className="useremail">
+          <input
+            className={styles.input}
+            type="text"
+            name="useremail"
+            id="useremail"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div className="usepassword">
+          <input
+            className={styles.input}
+            type="text"
+            name="usepassword"
+            id="usepassword"
+            placeholder="Enter your usepassword"
+          />
+        </div>
+      </div>
+      <div className={styles.privacy}>
+        <input type="checkbox" name="" id="" />
+        <a href="#">I agree to privacy policy & terms</a>
+      </div>
+      <div className={styles.ctabtn}>
+        <button>Continue</button>
+      </div>
+      <div className={styles.hrline}>
+        <div className={styles.line}></div>
+        <span>or</span>
+        <div className={styles.line}></div>
+      </div>
+    </div>
+  );
 }
+
+export default Login;
